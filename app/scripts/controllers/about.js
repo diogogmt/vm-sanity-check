796f1d21981c934c30547368eb7357901a2cd7f1
Diogo Monteiro <diogo.gmt@gmail.com>
2015-06-02 13:43:01 -0400

    Init commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vmSanityCheckApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vmSanityCheckApp
+ */
+angular.module('vmSanityCheckApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vmSanityCheckApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vmSanityCheckApp
+ */
+angular.module('vmSanityCheckApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
